0b157a9e324b72cd424c03b3ee3e9df30ae5f6ed
Andrea Vinzoni <andrea.vinzoni@gmail.com>
2021-01-30 20:17:46 +0100

    refactoring

--- src/main/java/TourneamentRound.java
+++ src/main/java/TourneamentRound.java
@@ -5,15 +5,13 @@ public class TourneamentRound {
 
     public static int GAMES_PER_ROUND = Configuration.players.length / 2;
     private static int nextGameNumber = 1;
-    private static TourneamentRound firstRound; // Primo turno precalcolato a beneficio dell'inizializzazione della UI.
+    private static final TourneamentRound firstRound; // Primo turno precalcolato a beneficio dell'inizializzazione della UI.
 
     static {
         firstRound = new TourneamentRound(1);
     }
 
-    private final List<PairingSystemSimilarRating.PlayersPair> pairings;
     private final Game[] games = new Game[GAMES_PER_ROUND];
-    private TourneamentRoundUI tourneamentRoundUI;
 
     public static TourneamentRound create(int roundNumber) {
         return roundNumber == 1 ? firstRound : new TourneamentRound(roundNumber);
@@ -21,7 +19,7 @@ public class TourneamentRound {
 
     private TourneamentRound(int roundNumber) {
 
-        pairings = Configuration.paringSystem.pairings(roundNumber);
+        List<PairingSystem.PlayersPair> pairings = Configuration.paringSystem.pairings(roundNumber);
 
         for (int i=0; i<GAMES_PER_ROUND; ++i ) {
             Player player1 = pairings.get(i).player1;
@@ -35,7 +33,6 @@ public class TourneamentRound {
     public Game[] games() { return games; }
 
     public void setUIView(TourneamentRoundUI tourneamentRoundUI) {
-       this.tourneamentRoundUI = tourneamentRoundUI;
 
        GameUI[] gamesUI = tourneamentRoundUI.gamesUI();
 
@@ -46,7 +43,6 @@ public class TourneamentRound {
     }
 
     public void run() throws InterruptedException {
-        GameUI[] gamesUI = tourneamentRoundUI.gamesUI();
 
         List<Thread> threads = new ArrayList<>();
         for (int i=0; i<TourneamentRound.GAMES_PER_ROUND; ++i) {
